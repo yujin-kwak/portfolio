85af6f84a5a6fc5155d2f9c3e13cdcfdbd12d66f
yujin-kwak <yujin02371@gmail.com>
2025-11-06 16:15:02 +0900

    feat: Home

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -2,11 +2,15 @@
 import React from "react";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Home from "@/components/Home";
+
 export default function PortfolioPage() {
   return (
     <div className="max-w-screen min-h-screen bg-gray-900">
       <Navbar />
-      <main></main>
+      <main>
+        <Home />
+      </main>
       <Footer />
     </div>
   );
---
+++ portfolio/src/components/Home.tsx
@@ -0,0 +1,19 @@
+const Home = () => (
+  <section
+    id="home"
+    className="min-h-screen flex flex-col justify-center items-center text-center p-4"
+  >
+    <h1 className="text-4xl md:text-6xl font-extrabold mb-4 leading-tight text-white text-glow">
+      성장을 즐기는 프론트엔드 개발자
+    </h1>
+    <p className="text-5xl md:text-7xl font-black mb-8 text-indigo-400">
+      곽유진입니다.
+    </p>
+    <p className="max-w-2xl text-lg md:text-xl text-gray-300">
+      새로운 기술을 배우고 적용하는 과정에서 즐거움을 느끼며, 동료들과의 소통을
+      통해 함께 성장하는 것을 중요하게 생각합니다.
+    </p>
+  </section>
+);
+
+export default Home;
