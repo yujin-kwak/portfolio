b73ada155bf433216cb73cb641c8b44337c5e012
yujin-kwak <yujin02371@gmail.com>
2025-11-07 13:00:07 +0900

    chore: EnvelopeIcon code edit

--- portfolio/src/components/icons/EnvelopeIcon.tsx
+++ portfolio/src/components/icons/EnvelopeIcon.tsx
@@ -4,6 +4,7 @@ type IconProps = {
 
 const EnvelopeIcon = ({ className }: IconProps) => (
   <svg
+    className={className}
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
